a44ddb1cbffd17f9bb288aa579325fa13ea9f1e0
Ковальков Александр <akovalkoff@yandex.ru>
2022-08-31 11:15:25 +0300

    Radioman

--- src/test/java/ru/netology/domain/RadiomanTest.java
+++ src/test/java/ru/netology/domain/RadiomanTest.java
@@ -15,8 +15,9 @@ class RadiomanTest {
         radio.setName(expected);
         assertEquals(expected, radio.getName());
     }
+
     @Test
-    void initFields(){
+    void initFields() {
         assertEquals(9, radio.getMaxStation());
         assertEquals(0, radio.getMinStation());
         assertEquals(100, radio.getMaxVolume());
@@ -39,33 +40,37 @@ class RadiomanTest {
         radio.setCurrentStation(10);
         assertEquals(9, radio.getCurrentStation());
     }
+
     @Test
-    void increaseStation(){
+    void increaseStation() {
         radio.setCurrentStation(3);
 
         radio.increaseStation();
-        assertEquals(4,radio.getCurrentStation());
+        assertEquals(4, radio.getCurrentStation());
     }
+
     @Test
-    void transitionFromMaxToMinStation(){
+    void transitionFromMaxToMinStation() {
         radio.setCurrentStation(9);
 
         radio.increaseStation();
-        assertEquals(0,radio.getCurrentStation());
+        assertEquals(0, radio.getCurrentStation());
     }
+
     @Test
-    void downStation(){
+    void downStation() {
         radio.setCurrentStation(2);
 
         radio.downStation();
-        assertEquals(1,radio.getCurrentStation());
+        assertEquals(1, radio.getCurrentStation());
     }
+
     @Test
-    void transitionFromMimToMaxStation(){
+    void transitionFromMimToMaxStation() {
         radio.setCurrentStation(0);
 
         radio.downStation();
-        assertEquals(9,radio.getCurrentStation());
+        assertEquals(9, radio.getCurrentStation());
     }
 
     @Test
@@ -75,6 +80,7 @@ class RadiomanTest {
         radio.setCurrentVolume(-1);
         assertEquals(5, radio.getCurrentVolume());
     }
+
     @Test
     void shouldNotSetVolumeAboveMax() {
         radio.setCurrentVolume(100);
@@ -82,33 +88,37 @@ class RadiomanTest {
         radio.setCurrentVolume(101);
         assertEquals(100, radio.getCurrentVolume());
     }
+
     @Test
-    void increaseVolume(){
+    void increaseVolume() {
         radio.setCurrentVolume(6);
 
         radio.increaseVolume();
-        assertEquals(7,radio.getCurrentVolume());
+        assertEquals(7, radio.getCurrentVolume());
     }
+
     @Test
-    void transitionFromMaxToMinVolume(){
+    void transitionFromMaxToMinVolume() {
         radio.setCurrentVolume(100);
 
         radio.increaseVolume();
-        assertEquals(0,radio.getCurrentVolume());
+        assertEquals(0, radio.getCurrentVolume());
     }
+
     @Test
-    void downVolume(){
+    void downVolume() {
         radio.setCurrentVolume(6);
 
         radio.downVolume();
-        assertEquals(5,radio.getCurrentVolume());
+        assertEquals(5, radio.getCurrentVolume());
     }
+
     @Test
-    void transitionFromMinToMaxVolume(){
+    void transitionFromMinToMaxVolume() {
         radio.setCurrentVolume(0);
 
         radio.downVolume();
-        assertEquals(100,radio.getCurrentVolume());
+        assertEquals(100, radio.getCurrentVolume());
     }
 
 
